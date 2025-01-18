98fac3c9c86ef57ef4b6ec758ad23be5b32ecd41
Rafael Moreira <150485884+Rafael217-dev@users.noreply.github.com>
2025-01-18 00:21:56 -0300

    Update uri1040(CERTO).java

--- pasta1/uri1040(CERTO).java
+++ pasta1/uri1040(CERTO).java
@@ -9,7 +9,12 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		float n1, n2, n3, n4, media, nota_exame, media_final;
-		
+
+		/*Esse aqui é o jeito correto de resolver o problema. A real é que as únicas diferenças de um para o outro é o uso do float, ao invés do double
+  		o que torna esse código um pouco mais leve que o outro, porém, menos preciso em suas contas matemáticas. Então, a dica é: Se precisa de uma
+    		api que não exija tanto uma precisão muito detalhada e precisa ser otimizada em questão de memória, como sistemas embarcados ou dispositivos
+      		com baixa capacidade, Float é a melhor opção. Agora, se você vai mexer com bancos, cálculos sensíveis, ciências e etc, o Duble é a melhor opção
+		mais pesado?? Sim, porém mais preciso e confiável.*/
 		
 		n1 = sc.nextFloat();
 		n2 = sc.nextFloat();
